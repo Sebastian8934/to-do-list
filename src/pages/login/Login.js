886902a2afe68a189f8d1d785fe5f9352a5b5593
juan sebastian <juan.quiroz@gse.com.co>
2025-02-04 11:02:22 -0500

    Add new functions

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -24,27 +24,68 @@ function Login() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
+  const validateField = (value, fieldName, regex, minLength, customErrorMessage) => {
+    if (value.trim() === '') {
+      return `El campo ${fieldName} no puede estar en blanco.`;
+    }
+
+    if (regex && !regex.test(value)) {
+      return customErrorMessage || `El campo ${fieldName} no cumple con el formato esperado.`;
+    }
+
+    if (value.length < minLength) {
+      return `El campo ${fieldName} debe tener al menos ${minLength} caracteres.`;
+    }
+
+    return null; // Indica que la validación fue exitosa
+  };
+
+  const validateForm = () => {
+    let isValid = true;
+
+    const userNameError = validateField(userName, 'Usuario', /^.+$/, 4);
+    if (userNameError) {
+      setErrorUser(userNameError);
+      isValid = false;
+    } else {
+      setErrorUser("");
+    }
+
+    const errorPassword = validateField(password, 'Contraseña', /^.+$/, 4);
+    if (errorPassword) {
+      setErrorPassword(errorPassword);
+      isValid = false;
+    } else {
+      setErrorPassword("");
+    }
+
+    // Puedes agregar más bloques de validaciones para otros campos si es necesario
+    return isValid;
+  }
+
   const handleSubmit = async () => {
     let body = {
       user:userName,
       password:password
-    }
-    let response = await dispatch(login(body));
-    // console.log(response);
-    switch (response.statusCode) {
-      case 200:  
-        navigate('/home');
-        break;    
-      default:
-        console.log("usuario o contraseña incorrecta");        
-        break;
-    }
+    };
+    let responseValidate = validateForm();
+     if (responseValidate) {
+       let response = await dispatch(login(body));
+       // console.log(response);
+       switch (response.statusCode) {
+         case 200:  
+           navigate('/home');
+           break;    
+         default:
+           setErrorPassword("usuario o contraseña incorrecta");        
+           break;
+       }
+     }
   }
 
   return (
     <>
-      <NavbarIndexComponent text={"Inicio"} url={"index"} />
-      
+      <NavbarIndexComponent text={"Inicio"} url={"index"} />      
       <Box sx={{width:"100%",height:"80vh"}}>
         <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center"  width={"100%"} height={"100%"} >
           <Card sx={{ maxWidth:"100%", minWidth:400, padding:3 }}>
