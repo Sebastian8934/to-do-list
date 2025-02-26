cdf0bf7af76675d507f72fa00e2274a26b9d2910
Lawliet <elcostexd995@gmail.com>
2025-02-26 12:00:16 -0500

    feat: Implemented CRUD of Role entity

---
+++ src/pages/roles/edit/EditRole.js
@@ -0,0 +1,109 @@
+import React,{ useState,useEffect } from 'react';
+
+//Material ui
+import { Button, TextField, Box, Container } from '@mui/material';
+
+//Alertas 
+import Swal from 'sweetalert2';
+
+//Actions
+import { updateRoleService } from "../../../store/action/roleAction";
+
+function EditRole({ setView, infoUpdate }) {
+    const [ inputDescription, setInputDescription ] = useState('');
+    const [ errorDescription, setErrorDescription ] = useState('');
+
+    const returnView = () => {
+      setView({ list:true });
+    }
+
+    useEffect(() => {
+      setInputDescription(infoUpdate.name);
+    }, []); 
+
+    const edit = async () => {
+        const body = {
+          id: infoUpdate._id,
+          name: inputDescription
+        }
+
+        if (validateForm()) {
+            let responseData = await updateRoleService(body);
+
+            if (responseData.statusCode === 200) {
+                showAlert(
+                    "Se edito con exito",
+                    `<p class="alert-success"> Se edito con exito. </p>`
+                );
+            } else {
+                showAlert(
+                    "A ocurrido un error",
+                    `<p class="alert-failed"> A ocurrido un error. </p>`
+                );
+            }
+          }
+    }
+
+    const validateForm = () => {
+        let isValid = true;
+  
+        const descriptionError = validateField(inputDescription, 'descripcion', /^.+$/, 4);
+
+        if (descriptionError) {
+          setErrorDescription(descriptionError);
+          isValid = false;
+        } else {
+          setErrorDescription("");
+        }
+  
+        return isValid;
+    }
+
+    const validateField = (value, fieldName, regex, minLength, customErrorMessage) => {
+        if (value.trim() === '') {
+            return `El campo ${fieldName} no puede estar en blanco.`;
+        }
+    
+        if (regex && !regex.test(value)) {
+            return customErrorMessage || `El campo ${fieldName} no cumple con el formato esperado.`;
+        }
+    
+        if (value.length < minLength) {
+            return `El campo ${fieldName} debe tener al menos ${minLength} caracteres.`;
+        }
+    
+        return null;
+    };
+
+    const showAlert = (type, message) => {
+      Swal.fire({
+          title: "Resultado",
+          html:`<p class="livness-alert-text font-poppins"> ${ type } </p>
+            ${ message }`,
+          confirmButtonText:"Finalizar",
+          customClass:{
+              popup:`livness-modal font-poppins`,
+              confirmButton:"livness-button",
+          },
+          allowOutsideClick:false
+      })
+    }
+
+    return (
+        <Container maxWidth="sm">
+        <Button onClick={ returnView } variant="contained">Volver</Button>
+            <Box sx={{
+            display:'flex',
+            justifyContent:'center',
+            flexDirection:'column',
+            alignItems:'center'
+            }}>
+            <TextField value={ inputDescription } onChange={(e) => setInputDescription(e.target.value)} sx={{ margin:"20px 0px 20px 0px" }} label="Descripcion" variant="outlined" />
+            {errorDescription && <p style={{ color: 'red' }}>{ errorDescription }</p>}
+            <Button onClick={ edit } variant="contained">Guardar</Button>
+            </Box>
+        </Container>
+    )
+}
+
+export default EditRole;
--- src/pages/roles/list/ListRoles.js
+++ src/pages/roles/list/ListRoles.js
@@ -1,6 +1,110 @@
+import React, { useState, useEffect } from 'react';
+
+//View
+import Create from '../create/CreateRole';
+import Edit from '../edit/EditRole';
+
+//Components
+import TableComponent from '../../../components/Table/TableComponent';
+import { Button, Box } from '@mui/material';
+
+//Alertas
+import Swal from 'sweetalert2';
+
+// Redux
+import { useSelector, useDispatch } from "react-redux";
+
+//Actions
+import { getRoleAllService, deleteRoleService } from "../../../store/action/roleAction";
+
 const ListRoles = () => {
+  const [ infoUpdate, setInfoUpdate ] = useState({});
+  const rows = useSelector((store) => store.role);
+  const dispatch = useDispatch();
+
+  const [ view, setView ] = useState({
+      list: true,
+      create: false,
+      update: false
+  });
+
+  useEffect(() => {
+    handleGet();
+  }, [dispatch, view]);
+
+  const columns = [{
+      field: 'name',
+      headerName: 'Descripcion',
+      width: 150,
+      editable: true,
+    }, {
+      field: "actions",
+      headerName: "Acciones",
+      width: 200,
+      renderCell: (params) => (
+        <div>
+          {/* Botón de Editar */}
+          <Button variant="contained" color="primary" size="small" style = { { marginRight: 10 } } onClick = { () => handleEdit(params.row) }>Editar</Button>
+          
+          {/* Botón de Eliminar */}
+          <Button variant="contained" color="secondary" size="small" onClick = { () => handleDelete(params.row._id) } > Eliminar </Button>
+        </div>
+      ),
+    }
+  ];
+
+  const handleGet = () => {
+    dispatch(getRoleAllService());
+  }
+
+  const handleCreate = () => {
+    setView({ create: true });
+  }
+
+  const handleEdit = (infoEdit) => {
+    setInfoUpdate(infoEdit);
+    setView({ update: true });
+  }
+
+  const handleDelete = async (id) => {
+        const responseData = await deleteRoleService(id);
+  
+        if (responseData.statusCode === 200) {
+          handleGet();
+          showAlert("Se elimino con exito", `<p class="alert-success"> Se elimino con exito. </p>`);
+        } else {
+          showAlert("A ocurrido un error", `<p class="alert-failed"> A ocurrido un error. </p>`);
+        }
+  }
+
+  const showAlert = (type, message) => {
+    Swal.fire({
+        title: "Resultado",
+        html:`<p class="livness-alert-text font-poppins"> ${ type } </p>
+          ${ message }`,
+        confirmButtonText:"Finalizar",
+        customClass:{
+            popup:`livness-modal font-poppins`,
+            confirmButton:"livness-button",
+        },
+
+        allowOutsideClick:false
+    });
+  }
+
   return (
-    <div>ListRoles</div>
+    <>
+      { view.list === true && (
+        <>
+          <Button variant="contained" onClick={ handleCreate }>Crear</Button>
+          <Box mt={ 2 }>
+            <TableComponent columns={ columns } rows={ rows } />
+          </Box>
+        </>
+      )}
+      { view.create === true && <Create setView={ setView } /> }
+      { view.update === true && <Edit setView={ setView } infoUpdate={ infoUpdate } /> }
+    </>
   )
 }
 
